78b8ce087a61993df1bcde34520455fc9a64a985
NyR01 <48245508+NyR01@users.noreply.github.com>
2019-03-05 12:40:13 +0100

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env   pyhton
+# __*__   coding:utf-8  __*__
+def main():
+    print("CONVERTIR UNA CANTIDAD DE TIEMPO EN SEGUNDOS A EL FORMATO HORAS-MINUTOS-SEGUNDOS")
+    segundos = int(input("Escriba una cantidad en segundos: "))
+    horas = segundos / 3600
+    minutos = (segundos % 3600) / 60
+    seg = 
+    
+    print("La media aritmética de {} y {} es {}".format(numero_1,numero_2,media))
+ 
+if __name__ == "__main__":
+    main()
+
