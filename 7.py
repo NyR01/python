85fac10bfcebb1ad629df8c2d0c5bbfdd7d61231
NyR01 <48245508+NyR01@users.noreply.github.com>
2019-03-25 13:11:58 +0100

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env  python
+#__*__  coding:utf-8  __*__
+def main():
+    print("ECUACIÓN A X + B = 0")
+    a = float(input("Escriba el coeficiente para a: "))
+    b = float(input("Escriba el coeficiente para b: "))
+
+
+    x = -b / a
+
+    print(x)
+
+if __name__ == "__main__":
+    main()
+
